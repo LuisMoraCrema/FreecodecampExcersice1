c1149ab48e4b67139dc1dbb582e123938328e31b
¨LuisMoraCrema¨ <mora.luis.lmg@gmail.com>
2025-12-04 01:11:41 -0300

    actualizacion3

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.use(helmet({
     }
   }
 }));
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;"
+  );
+  next();
+});
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
